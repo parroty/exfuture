9fd058818392615ce651452c7b9373f3c013a1d1
parroty <parroty00@gmail.com>
2013-12-26 11:24:40 +0900

    Update HttpServer to verify parallel html get

--- test/helper_test.exs
+++ test/helper_test.exs
@@ -40,12 +40,22 @@ defmodule ExFuture.HelperTest do
 
   test "parallel map for getting html pages" do
     HTTPotion.start
-    HttpServer.start(path: "/test", port: 4000, response: "Custom Response")
+    HttpServer.start(path: "/test", port: 4000,
+                     response: "Custom Response", wait_time: 1000)
 
+    # 10 requests, which each takes 1 second.
     links = List.duplicate("http://localhost:4000/test", 10)
+
+    s = :erlang.now
     htmls = links
               |> Enum.map(future(x) do HTTPotion.get(x).body end)
               |> Enum.map(resolve)
+
+    # It should take more than 1 second, but it should be less than
+    # 2 seconds, by the parallel execution.
+    time = :timer.now_diff(:erlang.now, s)
+    assert(time >= 1_000_000 and time <= 2_000_000)
+
     assert htmls == List.duplicate("Custom Response", 10)
   end
 end
